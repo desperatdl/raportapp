dd60eb69e8e8cf02e305942856c90c079131f134
Dawid <desperatdl@gmail.com>
2019-02-02 17:37:57 +0100

    added car and driver pickers history screen

--- app/src/main/java/pl/goinweb/dev/dawid/raportapp/MainActivity.java
+++ app/src/main/java/pl/goinweb/dev/dawid/raportapp/MainActivity.java
@@ -1,30 +1,109 @@
 package pl.goinweb.dev.dawid.raportapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.icu.lang.UScript;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    protected static Button next_button;
+    private ListView car_list, driver_list ;
+    protected static Button next_button, exit_button, car_button, driver_button, history_button;
+    public String choosenDriver, choosenCar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         onClickButtonListener();
+
     }
 
+    public void wybierzKierowce()
+    {
+        final CharSequence[] kierowcy = {"Darek", "Dawid", "Piotr"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Wybierz kierowce");
+        builder.setItems(kierowcy, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                driver_button.setText(kierowcy[item]);
+                choosenDriver = (String)(kierowcy[item]);
+//                Toast.makeText(MainActivity.this, choosenDriver, Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.create();
+        builder.show();
+    }
+
+    public void wybierzAuto()
+    {
+        final CharSequence[] auta = {"Merc", "Opel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Wybierz auto");
+        builder.setItems(auta, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                car_button.setText(auta[which]);
+                choosenCar = (String)(auta[which]);
+            }
+        });
+        builder.create();
+        builder.show();
+    }
     public void onClickButtonListener()
     {
         next_button = (Button)findViewById(R.id.next_button);
         next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("dataScreen");
+                Intent intent = new Intent(MainActivity.this, dataScreen.class);
                 startActivity(intent);
+            }
+        });
+
+        exit_button = (Button)findViewById(R.id.exit_button);
+        exit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        history_button = (Button)findViewById(R.id.history_button);
+        history_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, history.class);
+                startActivity(intent);
+            }
+        });
+
+        driver_button = (Button)findViewById(R.id.driver_button);
+        driver_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wybierzKierowce();
+            }
+        });
+
+        car_button = (Button)findViewById(R.id.car_button);
+        car_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wybierzAuto();
             }
         });
     }
--- app/src/main/java/pl/goinweb/dev/dawid/raportapp/dataScreen.java
+++ app/src/main/java/pl/goinweb/dev/dawid/raportapp/dataScreen.java
@@ -1,13 +1,43 @@
 package pl.goinweb.dev.dawid.raportapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class dataScreen extends AppCompatActivity {
+
+
+    protected Button next_button, back_button, check_button, save_button, data_start_button, data_stop_button, time_start_button, time_stop_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_screen);
+        OnClickButtonListener();
+    }
+
+    public void OnClickButtonListener()
+    {
+        next_button = (Button)findViewById(R.id.next_button);
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dataScreen.this, summary.class);
+                startActivity(intent);
+            }
+        });
+
+        back_button = (Button)findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
     }
 }
