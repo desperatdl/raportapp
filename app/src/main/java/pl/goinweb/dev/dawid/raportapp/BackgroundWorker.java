458c1c546bcddda7725f4c17887d326b7b857b3a
desperatdl <desperatdl@users.noreply.github.com>
2019-02-03 23:43:12 +0100

    Main screen connected to mysql

---
+++ app/src/main/java/pl/goinweb/dev/dawid/raportapp/BackgroundWorker.java
@@ -0,0 +1,87 @@
+package pl.goinweb.dev.dawid.raportapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class BackgroundWorker extends AsyncTask<String, Integer, Long> {
+
+    String line = null;
+    String result = null;
+    String resultCar = null;
+    String resultDriver = null;
+
+    String  driver_list_url = "http://dawid.dev.goinweb.pl/dawid/raport/androidapi/driver.html";
+    String  car_list_url = "http://dawid.dev.goinweb.pl/dawid/raport/androidapi/car.html";
+
+    @Override
+    protected Long doInBackground(String... params) {
+//        getUrl();
+//        String uri = params[0];
+        try {
+            URL url = new URL(driver_list_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream is = httpURLConnection.getInputStream();
+            resultDriver = readStream(is);
+            is.close();
+            httpURLConnection.disconnect();
+            URL url2 = new URL(car_list_url);
+            HttpURLConnection httpURLConnection2 = (HttpURLConnection) url2.openConnection();
+            InputStream is2 = httpURLConnection2.getInputStream();
+            resultCar = readStream(is2);
+            is2.close();
+            httpURLConnection2.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    return null;
+    }
+
+    private String readStream(InputStream is) {
+        // read content to string
+
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null)
+            {
+                sb.append(line+"\n");
+            }
+            br.close();
+            result = sb.toString();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+
+    }
+
+    @Override
+    protected void onPostExecute(Long aLong) {
+        super.onPostExecute(aLong);
+
+    }
+}
--- app/src/main/java/pl/goinweb/dev/dawid/raportapp/MainActivity.java
+++ app/src/main/java/pl/goinweb/dev/dawid/raportapp/MainActivity.java
@@ -9,47 +9,111 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView car_list, driver_list ;
+    BackgroundWorker bw = new BackgroundWorker();
     protected static Button next_button, exit_button, car_button, driver_button, history_button;
-    public String choosenDriver, choosenCar;
+    public String[] choosenDriver, choosenCar;
+    String car_list_url = "http://dawid.dev.goinweb.pl/dawid/raport/androidapi/car.html";
+    String driver_list_url = "http://dawid.dev.goinweb.pl/dawid/raport/androidapi/driver.html";
+//                Toast.makeText(MainActivity.this, choosenDriver, Toast.LENGTH_LONG).show();
+
+    public String lista_kierowcow;
+    public String lista_aut;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         onClickButtonListener();
+        bw.execute();
 
     }
 
-    public void wybierzKierowce()
+
+    public String[] pobierzListeAut()
     {
-        final CharSequence[] kierowcy = {"Darek", "Dawid", "Piotr"};
+        lista_aut = bw.resultCar;
+        String[] list = null;
+        // parse json
+
+        try {
+            JSONArray json = new JSONArray(lista_aut);
+
+            list = new String[json.length()];
+
+            //looping through all the elements in json array
+            for (int i = 0; i < json.length(); i++) {
+
+                //getting json object from the json array
+                JSONObject obj = json.getJSONObject(i);
+                list[i] = obj.getString("id") + "." + obj.getString("name");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public String[] pobierzListeKierowcow()
+    {
+        lista_kierowcow = bw.resultDriver;
+        String[] list = null;
+        // parse json
+
+        try {
+            JSONArray json = new JSONArray(lista_kierowcow);
+
+            list = new String[json.length()];
+
+            //looping through all the elements in json array
+            for (int i = 0; i < json.length(); i++) {
+
+                //getting json object from the json array
+                JSONObject obj = json.getJSONObject(i);
+                list[i] = obj.getString("id") + "." + obj.getString("name");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public void wybierzKierowce(final CharSequence[] data)
+    {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Wybierz kierowce");
-        builder.setItems(kierowcy, new DialogInterface.OnClickListener() {
+        builder.setItems(data, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int item) {
-                driver_button.setText(kierowcy[item]);
-                choosenDriver = (String)(kierowcy[item]);
-//                Toast.makeText(MainActivity.this, choosenDriver, Toast.LENGTH_LONG).show();
+                String string = (String)(data[item]);
+                String[] parts = string.split("\\.");
+                choosenDriver = parts;
+                driver_button.setText(parts[1]);
             }
         });
         builder.create();
         builder.show();
     }
 
-    public void wybierzAuto()
+    public void wybierzAuto(final CharSequence[] data)
     {
-        final CharSequence[] auta = {"Merc", "Opel"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Wybierz auto");
-        builder.setItems(auta, new DialogInterface.OnClickListener() {
+        builder.setItems(data, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                car_button.setText(auta[which]);
-                choosenCar = (String)(auta[which]);
+            public void onClick(DialogInterface dialog, int item) {
+                String string = (String)(data[item]);
+                String[] parts = string.split("\\.");
+                choosenCar = parts;
+                car_button.setText(parts[1]);
             }
         });
         builder.create();
@@ -88,7 +152,8 @@ public class MainActivity extends AppCompatActivity {
         driver_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wybierzKierowce();
+
+                wybierzKierowce(pobierzListeKierowcow());
             }
         });
 
@@ -96,7 +161,8 @@ public class MainActivity extends AppCompatActivity {
         car_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wybierzAuto();
+
+                wybierzAuto(pobierzListeAut());
             }
         });
     }
