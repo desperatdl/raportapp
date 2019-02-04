da87b1e0f90a1e8ea9fd7f05404d8a5b445d94f9
desperatdl <desperatdl@gmail.com>
2019-02-04 12:07:10 +0100

    scrooling

--- app/src/main/java/pl/goinweb/dev/dawid/raportapp/dataScreen.java
+++ app/src/main/java/pl/goinweb/dev/dawid/raportapp/dataScreen.java
@@ -1,5 +1,6 @@
 package pl.goinweb.dev.dawid.raportapp;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
@@ -9,19 +10,18 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 
-import java.sql.Time;
 import java.util.Calendar;
-import java.util.Date;
 
 
 public class dataScreen extends AppCompatActivity {
 
 
-    static public Button next_button, back_button, check_button, save_button, data_start_button, data_stop_button, time_start_button, time_stop_button;
-    static public Time choosen_time_start, choosen_time_stop;
-    static public Date choosen_date_start, choosen_date_stop
+    static public Button next_button, back_button, check_button, save_button, date_start_button, date_stop_button, time_start_button, time_stop_button;
+    static public String choosen_datetime_start, choosen_datetime_stop;
+    static public String time_start, time_stop, date_start, date_stop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +57,15 @@ public class dataScreen extends AppCompatActivity {
             }
         });
 
+        time_stop_button = findViewById(R.id.time_stop_button);
+        time_stop_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimeStopPicker();
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+            }
+        });
+
     }
 
     public static class TimeStartPicker extends DialogFragment
@@ -68,7 +77,6 @@ public class dataScreen extends AppCompatActivity {
             final Calendar c = Calendar.getInstance();
             int hour = c.get(Calendar.HOUR_OF_DAY);
             int minute = c.get(Calendar.MINUTE);
-
             // Create a new instance of TimePickerDialog and return it
             return new TimePickerDialog(getActivity(), this, hour, minute,
                     DateFormat.is24HourFormat(getActivity()));
@@ -76,9 +84,73 @@ public class dataScreen extends AppCompatActivity {
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             // Do something with the time chosen by the user
-            time_start_button.setText(hourOfDay + " : " + minute);
-            choosen_time_start = Time;
+            time_start =  hourOfDay + " : " + minute;
+            time_start_button.setText(time_start);
         }
     }
 
+    public static class TimeStopPicker extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            time_stop =  hourOfDay + " : " + minute;
+            time_stop_button.setText(time_stop);
+        }
+    }
+
+    public static class DateStartPicker extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            date_start = year+"-"+month+"-"+day;
+            date_start_button.setText(date_start);
+        }
+    }
+
+    public static class DateStopPicker extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            date_stop = year+"-"+month+"-"+day;
+            date_stop_button.setText(date_stop);
+        }
+    }
 }
